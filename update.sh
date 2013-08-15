ec0d7f7b571dfc1c8bfed2935276373a124e4aaa
Peter Zhang <peterzhang2016@gmail.com~>
2013-08-15 08:46:25 -0400

    revert to state with submodules

--- update.sh
+++ update.sh
@@ -1,8 +1,3 @@
 
 git pull origin master
-
-mkdir -p ~/.vim/autoload ~/.vim/bundle; \
-	curl -Sso ~/.vim/autoload/pathogen.vim \
-	    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-
 git submodule foreach git pull origin master
